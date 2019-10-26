0aec115fddc93497f00533375ce137f53224d87e
fybits <sergeig1313@gmail.com>
2019-10-26 13:36:38 +0300

    adding userids to database

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const crypto = require('crypto')
-var cookieParser = require('cookie-parser')
+const MongoClient = require('mongodb').MongoClient
+
+const mongoClient = new MongoClient("mongodb://localhost:27017/", {useNewUrlParser: true, useUnifiedTopology: true})
 
 const port = 8000;
 
 const app = express()
 app.use(cookieParser())
 
-
 app.get('/', (request, response) => {
     console.log("User connected!")
+    if (!request.cookies['userid']) {
+        let userid = crypto.randomBytes(20).toString('hex')
+        mongoClient.connect((err, client) => {
+            const db = client.db('admin')
+            const collection = db.collection('users')
+            collection.insertOne({'userid': userid})
+
+            mongoClient.close()
+        })
+        response.cookie("userid", userid)
+    }
     console.log("remote address: "+request.ip)
     console.log("cookies: "+JSON.stringify(request.cookies))
-    if (!request.cookies['userid'])
-        response.cookie("userid", crypto.randomBytes(20).toString('hex'))
+    
     response.send("Hello world from express!")
 })
 
