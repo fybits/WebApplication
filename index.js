a9e47467e6aa28c7ebf1a57337670ca4d556b8fc
fybits <sergeig1313@gmail.com>
2019-11-07 16:18:34 +0300

    authorization with password

--- index.js
+++ index.js
@@ -14,26 +14,26 @@ app.use(bodyParser.urlencoded({'extended': true}))
 
 app.get('/', (request, response) => {
     console.log("User connected!")
-    if (!request.cookies || request.cookies['userid'] == '') {
-        response.send('<form method="POST" action="register"><input name="nickname" value=""/><input value="Register" type="submit"/></form>')
+    if (!request.cookies.userid) {
+        console.log('Not authorized user')
+        response.sendFile('html/loginpage.html',{root:'.'})
     } else {
         mongoClient.connect((err, client) => {
             const db = client.db('admin')
             const collection = db.collection('users')
-            collection.findOne({'userid': request.cookies['userid']}, (err, result) => {
+            collection.findOne({'userid': request.cookies.userid}, (err, result) => {
                 if (err) {
-                    console.log(err)
                     response.send(err)
-                } else {
-                    console.log(result)
-                    response.send(`<p>Welcome, ${result['nickname']}!</p><a href="/exit">exit</a>`)
+                } else if (result) {
+                    // response.sendFile('html/home.html', {root:'.'})
+                    response.send(`<p>Welcome, ${result.nickname}!</p><a href="/exit">exit</a>`)
                 }
                 client.close()
             })
         })
     }
-    console.log("remote address: "+request.ip)
-    console.log("cookies: "+JSON.stringify(request.cookies))
+    console.log("remote address: ",request.ip)
+    console.log("cookies: ",request.cookies)
     
     //response.send("Hello world from express!")
 })
@@ -44,22 +44,45 @@ app.get('/exit', (request,response) => {
 
 })
 
+app.post('/login', (request,response) => {
+    mongoClient.connect((err, client) => {
+        const db = client.db('admin')
+        const collection = db.collection('users')
+        collection.findOne({'nickname': request.body.nickname}, (err, result) => {
+            if (result) {
+                if (crypto.createHash('md5').update(request.body.password).digest('hex') == result.password) {
+                    response.cookie("userid", result.userid)
+                    response.redirect('/')
+                } else {
+                    response.send('<p>Wrong password.<p>')
+                }
+            } else {
+                response.send(`<p>There is no user with name ${request.body.nickname}.</p>`)
+            }
+        })
+    })
+})
+
+
 app.post('/register', (request,response) => {
     let userid = crypto.randomBytes(20).toString('hex')
     mongoClient.connect((err, client) => {
         const db = client.db('admin')
         const collection = db.collection('users')
-        collection.findOne({'nickname': request.body['nickname']}, (err, result) => {
-            console.log(result)
+        collection.findOne({'nickname': request.body.nickname}, (err, result) => {
             if (result){
                 response.send('<p>Name already occupied!</p><a href="/">try again</a>')
             } else {
-                response.cookie("userid", userid)
-                collection.insertOne({'userid': userid, 'nickname': request.body['nickname']})
-                response.redirect('/')
+                if (request.body.password.length > 6) {
+                    response.cookie("userid", userid)
+                    collection.insertOne({'userid': userid, 'nickname': request.body.nickname, 'password': crypto.createHash('md5').update(request.body.password).digest('hex')})
+                    response.redirect('/')
+                } else {
+                    response.send('<p> password must be at least 6 characters long!</p>')
+                }
             }
         })
-        client.close()
+        //client.close()
     })
 })
 
@@ -69,3 +92,4 @@ app.listen(8000, (err) => {
     }
     console.log(`Server is listening on port ${port}`)
 })
+  
